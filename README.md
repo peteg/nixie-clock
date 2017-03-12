67c962fd5fcc5bac8b060076a21844bd38536fc1
Peter Gammie <peteg42@gmail.com>
2017-03-12 19:35:09 +1100

    First cut at getting rpmsg and the multiplexing to work together.

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-A BeagleBone Black and PRU driver for a nixie clock.
+# A BeagleBone Black and PRU driver for a nixie clock
 
-= Background
+
+## Background
 
 This sequence of posts is useful:
 
 https://www.zeekhuge.me/post/a_handfull_of_commands_and_scripts_to_get_started_with_beagleboneblack/
 
 
-
-
-= Complications
+## Complications
 
 Use PRU0 and header P9 rather than PRU1 and P8
  - the pins we want to use on P8 seem to affect the boot up process of the BBB
@@ -17,7 +16,8 @@ Use PRU0 and header P9 rather than PRU1 and P8
  - only use the lower 8 bits of PRU0 GPIOs (those on P9)
   - the rest are on P8 and share with the eMMC.
 
-= Licence
+
+## Licence
 
 All files without explicit copyright notices are covered by the
 licence in LICENCE.
--- pru_src/main_pru0.c
+++ pru_src/main_pru0.c
@@ -1,12 +1,55 @@
 /*
  * FIXME describe
+ *
+ * FIXME: perhaps halt other PRU?
  */
+
+#include <stdlib.h>
 
 #include <stdbool.h>
 #include <stdint.h>
+
 #include <pru_cfg.h>
 
+#include <pru_intc.h>
+#include <rsc_types.h>
+#include <pru_rpmsg.h>
+
+// FIXME cleanup
 #include "resource_table_pru0.h"
+#include "pru_defs.h"
+
+/* PRU GPIO registers. */
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+// printf is too big for the PRU. Library?
+char *
+itoa(int i, char *b)
+{
+  char *p = b;
+  int j;
+
+  if(i < 0) {
+    *p = '-';
+    p++;
+    i *= -1;
+  }
+
+  p++;
+  for(j = i; j != 0; j /= 10) {
+    p++;
+  };
+  *p = '\0';
+
+  do {
+    *--p = '0' + i % 10;
+    i /= 10;
+  } while(i);
+
+  return b;
+}
+
 
 /* FIXME shared with the ARM controller? */
 
@@ -36,8 +79,6 @@ ht_off(void)
 
 /* **************************************** */
 
-volatile register uint32_t __R30;
-
 /* The Russian 74141-equiv is hooked up to the lowest nibble of the
  * PRU0 GPIO pins. The next greatest nibble sets which tube is on
  * (one-hot). */
@@ -53,12 +94,17 @@ tubes_all_off(void)
 }
 
 static inline void
-tubes_set_val(digit_t digit, digit_val_t v)
+tubes_set_val(digit_t digit, digit_val_t v, unsigned int delay)
 {
   __R30 = DIGIT_VAL(v);
   /* FIXME sleep here long enough for the 74141-equiv to settle. 100us? */
-  __delay_cycles(200000);
+  __delay_cycles(20000);
   __R30 |= TUBE_VAL(digit);
+
+  /* FIXME cheesy hack: __delay_cycles requires a constant */
+  while(delay-- > 0) {
+    __delay_cycles(10000);
+  }
 }
 
 /* **************************************** */
@@ -90,7 +136,7 @@ digit_data[NUM_DIGITS]; // FIXME initialize?
 static inline void
 display_set_pending_val(digit_t digit, unsigned int new_val)
 {
-  digit_data[digit].pending_val = new_val;
+  digit_data[NUM_DIGITS - digit - 1].pending_val = new_val;
 }
 
 static inline uint8_t
@@ -135,8 +181,7 @@ digit_task(digit_t digit)
       d->digit_state = d->pending_val & FLASHING ? ds_flashing_fade_out : ds_crossfading;
       d->fade_index = 0;
     }
-    tubes_set_val(digit, d->displayed_val);
-    __delay_cycles(DELAY_MAXIMUM * 1000000); // FIXME abstract
+    tubes_set_val(digit, d->displayed_val, DELAY_MAXIMUM);
     break;
 
   case ds_crossfading:
@@ -146,10 +191,8 @@ digit_task(digit_t digit)
       d->displayed_val = d->next_val;
       d->digit_state = ds_steady;
     }
-    tubes_set_val(digit, d->displayed_val);
-    __delay_cycles(delay * 1000000);
-    tubes_set_val(digit, d->next_val);
-    __delay_cycles((DELAY_MAXIMUM - delay) * 1000000);
+    tubes_set_val(digit, d->displayed_val, delay);
+    tubes_set_val(digit, d->next_val, DELAY_MAXIMUM - delay);
     break;
 
   case ds_flashing_fade_out:
@@ -160,11 +203,9 @@ digit_task(digit_t digit)
       /* Pick up a new value if any. */
       d->displayed_val = d->next_val;
     } else {
-      tubes_set_val(digit, d->displayed_val);
-      __delay_cycles(delay * 1000000);
+      tubes_set_val(digit, d->displayed_val, delay);
     }
-    tubes_set_val(digit, BLANK_DIGIT);
-    __delay_cycles((DELAY_MAXIMUM - delay) * 1000000);
+    tubes_set_val(digit, BLANK_DIGIT, DELAY_MAXIMUM - delay);
     break;
 
   case ds_flashing_fade_in:
@@ -177,11 +218,9 @@ digit_task(digit_t digit)
         d->digit_state = ds_flashing_fade_out;
       }
     } else {
-      tubes_set_val(digit, d->displayed_val);
-      __delay_cycles(delay * 1000000);
+      tubes_set_val(digit, d->displayed_val, delay);
     }
-    tubes_set_val(digit, BLANK_DIGIT);
-    __delay_cycles((DELAY_MAXIMUM - delay) * 1000000);
+    tubes_set_val(digit, BLANK_DIGIT, DELAY_MAXIMUM - delay);
     break;
   }
 
@@ -199,40 +238,107 @@ display_init(void)
     d->next_val = BLANK_DIGIT;
     d->pending_val = 0;
   }
+}
+
+/* **************************************** */
+/* RPMSG communication with the ARM host. */
+
+// FIXME only one instance so just use globals
+
+// FIXME cannot be defined inside main (on the stack?)
+static uint8_t payload[RPMSG_BUF_SIZE];
+static struct pru_rpmsg_transport transport;
+
+#define HOST_ARM_TO_PRU0 HOST0_INT
+
+static void
+rpmsg_init(void)
+{
+  volatile uint8_t *status;
+
+  /* allow OCP master port access by the PRU so the PRU can read external memories */
+  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+  /* clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us */
+  CT_INTC.SICR_bit.STS_CLR_IDX = SE_ARM_TO_PRU0;
+
+  /* Make sure the Linux drivers are ready for RPMsg communication */
+  status = &resourceTable.rpmsg_vdev.status;
+  while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
+
+  /* Initialize pru_virtqueue corresponding to vring0 (PRU to ARM Host direction) */
+  pru_virtqueue_init(&transport.virtqueue0, &resourceTable.rpmsg_vring0, SE_PRU0_TO_ARM, SE_ARM_TO_PRU0);
+
+  /* Initialize pru_virtqueue corresponding to vring1 (ARM Host to PRU direction) */
+  pru_virtqueue_init(&transport.virtqueue1, &resourceTable.rpmsg_vring1, SE_PRU0_TO_ARM, SE_ARM_TO_PRU0);
+
+  /* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
+  while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, RPMSG_CHAN_NAME, PRU0_RPMSG_CHAN_DESC, PRU0_RPMSG_CHAN_PORT) != PRU_RPMSG_SUCCESS);
+}
+
+static bool
+rpmsg_poll(void)
+{
+  /* Check bit 30 of register R31 to see if the ARM has kicked us */
+  if(check_host_int(HOST_ARM_TO_PRU0)) {
+    uint16_t src, dst, len;
+
+    /* Clear the event status */
+    CT_INTC.SICR_bit.STS_CLR_IDX = SE_ARM_TO_PRU0;
+
+    /* Receive all available messages, multiple messages can be sent per kick */
+    while(pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
+      static uint32_t xxx = 0;
+      char *buf = "xxxxxxxxxx";
+
+      itoa(xxx, buf);
+      xxx++;
+      pru_rpmsg_send(&transport, dst, src, buf, 10);
+      pru_rpmsg_send(&transport, dst, src, payload, len);
+
+      for(int i = 0; i < 4; i++) {
+        // FIXME take care of flashing top bit
+        display_set_pending_val(i, payload[i] - '0');
+      }
+
+      ht_off();
+      tubes_all_off();
+      /* FIXME set the PRU GPIO to output if we can. */
+      tubes_all_off();
+      ht_on();
+
+    }
+
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/* **************************************** */
+// FIXME experiments
+
+void
+main(void)
+{
+  ht_off();
+  tubes_all_off();
+  ht_on();
+
+  display_init();
+  rpmsg_init();
 
   /* FIXME initial display */
   for(unsigned int i = 0; i < NUM_DIGITS; i++) {
     display_set_pending_val(i, i);
   }
-}
 
-/* **************************************** */
-
-void
-main(void)
-{
-  int vals[4] = {1, 2, 3, 4};
-
-  ht_off();
-  tubes_all_off();
-  /* FIXME set the PRU GPIO to output if we can. */
-  tubes_all_off();
-  ht_on();
-
-  while (1) {
-    for(int i = 0; i < 100; i++) {
-      for(int j = 0; j < 4; j++) {
-        __R30 = DIGIT_VAL(vals[j]) | TUBE_VAL(j);
-        __delay_cycles(100000);
-      }
-    }
-    for(int j = 0; j < 4; j++) {
-      vals[j] = (vals[j] + 1) % 10;
+  while(1) {
+    for(int i = 0; i < NUM_DIGITS; i++) {
+      rpmsg_poll();
+      digit_task(i);
     }
   }
-
-  tubes_all_off();
-  ht_off();
 
   __halt();
 }
